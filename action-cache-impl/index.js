e99b27d6e1482ce03b3b1e7f3f5b8aeafa9ab36a
Jon Jensen <jenseng@gmail.com>
2023-12-06 17:59:35 -0800

    debug input oddity

---
+++ action-cache-impl/index.js
@@ -0,0 +1,2 @@
+console.log(`action-cache-impl path = ${process.env["INPUT_PATH"]} (main)`);
+if (process.env["INPUT_PATH"] === ".") process.exit(1);
---
+++ action-cache-impl/post.js
@@ -0,0 +1,2 @@
+console.log(`action-cache-impl path = ${process.env["INPUT_PATH"]} (post)`);
+if (process.env["INPUT_PATH"] === ".") process.exit(1);
